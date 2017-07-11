e0d040f3217b3d66533d6d08248b312a9d2f85ea
aniruthp <AParthasarathy@carnival.com>
2017-07-11 10:42:15 -0400

    Included SpringDataParam based on the issue I raised on github https://github.com/springfox/springfox/issues/1882

---
+++ sb-ref-reservation-service/src/main/java/com/example/reservation/config/SpringDataParamBuilderPlugin.java
@@ -0,0 +1,38 @@
+package com.example.reservation.config;
+
+import com.google.common.base.Optional;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
+import springfox.documentation.service.ResolvedMethodParameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.ParameterBuilderPlugin;
+import springfox.documentation.spi.service.contexts.ParameterContext;
+import springfox.documentation.swagger.common.SwaggerPluginSupport;
+
+/**
+ * Created by a.c.parthasarathy on 7/11/17.
+ * This class is required to
+ */
+@Component
+@Order(SwaggerPluginSupport.SWAGGER_PLUGIN_ORDER + 1000)
+public class SpringDataParamBuilderPlugin implements ParameterBuilderPlugin {
+    @Override
+    public void apply(ParameterContext parameterContext) {
+        ResolvedMethodParameter methodParameter = parameterContext.resolvedMethodParameter();
+        Optional<Param> requestParam = methodParameter.findAnnotation(Param.class);
+        if (requestParam.isPresent()) {
+            parameterContext.parameterBuilder()
+                    .parameterType("query")
+                    .name(requestParam.get().value())
+                    .description(requestParam.get().value());
+        }
+    }
+
+    @Override
+    public boolean supports(DocumentationType documentationType) {
+        return true;
+    }
+}
+
+
